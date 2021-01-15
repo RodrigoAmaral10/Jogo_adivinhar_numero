60538802493bbd5caba12c1841cb23e0cafabed9
RodrigoAmaral10 <rodrigo.dsn.imb@gmail.com>
2021-01-14 21:42:53 -0300

    Alterada quantidade de números para 100

--- adivinhacao.py
+++ adivinhacao.py
@@ -11,8 +11,8 @@ for rodada in range(1, total_de_tentativas + 1):
     print("Você digitou " , chute_str)
     chute = int(chute_str)
 
-    if(chute < 1 or chute > 50):
-        print("Você deve digitar um número entre 1 e 50!")
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
         continue
 
     acertou = chute == numero_secreto
